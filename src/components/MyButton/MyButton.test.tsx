77a2fbb0589365dd71a62a717ec39e81246e60b6
phadhiil001 <phadhiil001@gmail.com>
2024-07-11 12:10:14 -0500

    Testing CI one more time

--- src/components/MyButton/MyButton.test.tsx
+++ src/components/MyButton/MyButton.test.tsx
@@ -2,6 +2,6 @@ import "@testing-library/jest-dom";
 // import { expect, test } from "@jest/globals";
 import { sum } from "./MyButton";
 
-test("add 1 + 2 to equal 3", () => {
-  expect(sum(1, 2)).toBe(3);
+test("add 2 + 2 to equal 4", () => {
+  expect(sum(2, 2)).toBe(4);
 });
